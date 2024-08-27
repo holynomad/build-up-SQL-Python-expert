7e860f9b1966efe6c5c11ddefb8ffdc51f45cad3
Macgyver Lee <43846264+holynomad@users.noreply.github.com>
2024-08-27 23:58:26 +0900

    Update 240827.sql

--- 240827.sql
+++ 240827.sql
@@ -1,4 +1,5 @@
-# DEVELOPER_INFOS 테이블에서 Python 스킬을 가진 개발자의 정보를 조회하려 합니다. Python 스킬을 가진 개발자의 ID, 이메일, 이름, 성을 조회하는 SQL 문을 작성해 주세요.
+# DEVELOPER_INFOS 테이블에서 Python 스킬을 가진 개발자의 정보를 조회하려 합니다. 
+# Python 스킬을 가진 개발자의 ID, 이메일, 이름, 성을 조회하는 SQL 문을 작성해 주세요.
 # 결과는 ID를 기준으로 오름차순 정렬해 주세요.
 
 select  ID
