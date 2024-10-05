0affb6478b219e87cd74990059e020a1377ec5d3
Se Ha Lee <43846264+holynomad@users.noreply.github.com>
2024-10-05 23:53:34 +0900

    Update 241003_lv2.sql for real query added

--- 241003_lv2.sql
+++ 241003_lv2.sql
@@ -1,3 +1,11 @@
 # PRODUCT 테이블과 OFFLINE_SALE 테이블에서 상품코드 별 매출액(판매가 * 판매량) 합계를 출력하는 SQL문을 작성해주세요. 
 # 결과는 매출액을 기준으로 내림차순 정렬해주시고 매출액이 같다면 상품코드를 기준으로 오름차순 정렬해주세요.
 
+select p.PRODUCT_CODE
+     , sum(p.PRICE * os.SALES_AMOUNT) as SALES
+  from PRODUCT p
+  join OFFLINE_SALE os on p.PRODUCT_ID = os.PRODUCT_ID
+ group by p.PRODUCT_CODE
+ order by SALES desc
+        , p.PRODUCT_CODE
+ ;
