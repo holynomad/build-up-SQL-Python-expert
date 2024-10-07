d35ef34c3c7ea632db591f44fe612ab4fc808312
Se Ha Lee <43846264+holynomad@users.noreply.github.com>
2024-10-07 23:24:25 +0900

    Update 241005_lv2.py for real code

--- 241005_lv2.py
+++ 241005_lv2.py
@@ -28,3 +28,19 @@ function solution(s):
         return 1
     else:
         return 0
+
+# solution
+def solution(s):
+    stack = []
+    
+    for char in s:
+        if stack and stack[-1] == char:
+            stack.pop()
+        else:
+            stack.append(char)
+    
+    return 1 if not stack else 0
+
+# 테스트
+print(solution("baabaa"))  # 1 
+print(solution("cdcd"))    # 0 
