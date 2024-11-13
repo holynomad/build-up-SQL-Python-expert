7b9a07107519a198a88264cff440c18ba5522004
Se Ha Lee <43846264+holynomad@users.noreply.github.com>
2024-11-13 16:32:49 +0900

    Update 202411_lv3.sql for 13th Nov, 2024

--- 202411_lv3.sql
+++ 202411_lv3.sql
@@ -10,3 +10,16 @@ select distinct crcc.CAR_ID
  order by crcc.CAR_ID desc
  ;
    
+# 11/13 (수)
+# 관리자의 실수로 일부 동물의 입양일이 잘못 입력되었습니다. 
+# 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름을 조회하는 SQL문을 작성해주세요. 
+# 이때 결과는 보호 시작일이 빠른 순으로 조회해야합니다.
+# 본 문제는 Kaggle의 "Austin Animal Center Shelter Intakes and Outcomes"에서 제공하는 데이터를 사용하였으며 ODbL의 적용을 받습니다.
+
+select ao.ANIMAL_ID
+     , ao.NAME
+  from ANIMAL_OUTS ao
+  join ANIMAL_INS ai on ao.ANIMAL_ID = ai.ANIMAL_ID
+ where ao.DATETIME < ai.DATETIME
+ order by ai.DATETIME asc
+ ;
