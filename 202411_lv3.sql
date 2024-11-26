d9f5d2ad3a2f4156eb31e69f0fbb2ff0de5a7783
Se-Ha Lee <43846264+holynomad@users.noreply.github.com>
2024-11-26 21:31:18 +0900

    Update 202411_lv3.sql for 26th Nov 2024

--- 202411_lv3.sql
+++ 202411_lv3.sql
@@ -83,3 +83,9 @@ where r1.FAVORITES = (
      where r2.FOOD_TYPE = r1.FOOD_TYPE
 )
 order by r1.FOOD_TYPE desc;
+
+
+# 11/26 (화)
+# 천재지변으로 인해 일부 데이터가 유실되었습니다. 
+# 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물의 ID와 이름을 ID 순으로 조회하는 SQL문을 작성해주세요.
+
